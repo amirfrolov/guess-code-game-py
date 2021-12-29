8783efc932c357f03f0aa49db21a139e379e9713
amirfrolov <47364930+amirfrolov@users.noreply.github.com>
2021-12-29 21:08:06 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
+import os
+from os.path import abspath #for opening files
+os.chdir(os.path.dirname(__file__)) #set the path for opening files
 from collections import deque, Counter
 import random
 
 CODE_SIZE = 4
-INSTRUCTIONS = """
-Enter a 4 digit number to guess the secret code.
-The code is made of 4 diffrent digits from 0-9.
-After every attempt you will get the number of digits
-you found are in the right place in the secret code (direct hits)
-And the number of digits you found are in the secret code
-but not in the right place (not direct hits).
-the goal is to guess the secret code.
-good luck!
-"""
+INSTRUCTIONS_FILE = "instructions.txt"
 
 #colors - https://www.geeksforgeeks.org/print-colors-python-terminal/
 class bcolors:
@@ -21,9 +15,10 @@ class bcolors:
     lightblue='\033[94m'
     green='\033[32m'
     ENDC = '\033[0m'
-    BOLD = '\033[1m'
+    BOLDget_secret_code = '\033[1m'
     UNDERLINE = '\033[4m'
-    
+
+#returns a random secret code
 def get_secret_code():
     result = deque()
     digits = deque([i for i in range(10)])
@@ -33,6 +28,7 @@ def get_secret_code():
         del digits[digit_index]
     return [str(i) for i in result]
 
+#checks if all the characters in the string are present only once.
 def is_unique_chars(string):
 	# Counting frequency
 	freq = Counter(string)
@@ -42,6 +38,7 @@ def is_unique_chars(string):
 	else:
 		return False
 
+#get a code guess from the user, if user guess invalid try again
 def get_code_guess():
     error_flag = True
     while error_flag:
@@ -51,20 +48,23 @@ def get_code_guess():
         if len(code_guess) != CODE_SIZE:
             error_flag = True
             print("Guess length is not 4 digits, Try again.")
-        if not error_flag and not code_guess.isdigit():
+        elif not code_guess.isdigit():
             error_flag = True
             print("Guess does not contain only digits, Try again.")
-        if not error_flag and not is_unique_chars(code_guess):
+        elif not is_unique_chars(code_guess):
             error_flag = True
             print("Guess does not contain only unique digits, Try again.")
     return code_guess
 
 #   ----------Main----------
 def main():
-    print(INSTRUCTIONS)
     secret_code =  get_secret_code()
     win = False
     attempts = 0
+    #print instructions
+    with open(abspath(INSTRUCTIONS_FILE), 'r') as file_handle:
+        print(file_handle.read())
+    #main code
     while not win:
         attempts += 1
         print(f"Attempt {attempts}: ", end="")
@@ -72,20 +72,22 @@ def main():
         #change print color
         print(end=bcolors.green)
         print(code_guess)
-        same_digits = 0
-        same_place = 0
+        digit_exists_counter = 0
+        same_place_counter = 0
         for i in range(CODE_SIZE):
+            #if the digit is in the right place
             if secret_code[i] == code_guess[i]:
-                same_place += 1
+                same_place_counter += 1
+            #if the digit is not in the right place exists
             elif secret_code[i] in code_guess:
-                same_digits += 1
+                digit_exists_counter += 1
         
-        if same_place == CODE_SIZE:
+        if same_place_counter == CODE_SIZE:
             win = True
             print(f"You won! After {attempts} attempts.")
         else:
-            print(f"Direct hits:     {same_place}")
-            print(f"Not direct hits: {same_digits}")
+            print(f"Direct hits:     {same_place_counter}")
+            print(f"Not direct hits: {digit_exists_counter}")
         #restore print color
         print(end=bcolors.ENDC)
     return None
